950444e56187a59747d118ea21ffdc289788aaa2
Alecio <aleciosilveira97@gmail.com>
2025-08-30 01:18:36 -0300

    Token Service

---
+++ src/main/java/com/petShot/clitente/infra/secutiry/TokenService.java
@@ -0,0 +1,52 @@
+package com.petShot.clitente.infra.secutiry;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.petShot.clitente.entity.EntityCliente;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("$api.security.token.secret")
+    private String secret;
+
+    public String gerarToken(EntityCliente entityCliente) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("Api petShop")
+                    .withSubject(entityCliente.getEmail())
+                    .withClaim("id", entityCliente.getId())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algorithm);
+
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro ao gerar token" + exception);
+        }
+    }
+
+    public String getSubject(String tokenJwt) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("Api petShop")
+                    .build()
+                    .verify(tokenJwt)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT expirado ou invalido!" + tokenJwt);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
